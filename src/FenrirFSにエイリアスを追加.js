225b0c4213fb965ac0247697a4da5b4593e67141
perorin <perorin@MyComputer>
2018-08-26 22:29:17 +0900

    プロジェクト作成

---
+++ src/FenrirFSにエイリアスを追加.js
@@ -0,0 +1,24 @@
+(function() {
+
+// FenrirFS.exe のパス
+// 64 bit
+// var FenrirFSPath = "......\\FenrirFS.exe"
+
+// 32 bit
+var FenrirFSPath = "......\\FenrirFS.exe"
+
+// プロファイル名
+var ProfileName = "個人用";
+
+var args = WScript.Arguments;
+if ( args.length <= 0 ) {
+  return;
+}
+
+var wsh = new ActiveXObject("WScript.Shell");
+for(var i=0; i<args.length; i++){
+  var cmdline = '"' + FenrirFSPath + '" --profile "' + ProfileName + '" --alias "' + args(i) + '"';
+  wsh.Run(cmdline);
+}
+
+})();
