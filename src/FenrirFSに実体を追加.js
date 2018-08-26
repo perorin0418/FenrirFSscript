b42c2e10c217223e72bdbfd7db13807b7949a1cd
perorin <perorin@MyComputer>
2018-08-27 00:37:25 +0900

    フォルダーはショートカットを作成して取り込むようにする

--- src/FenrirFSに実体を追加.js
+++ src/FenrirFSに実体を追加.js
@@ -1,24 +1,51 @@
 (function() {
 
-// FenrirFS.exe のパス
-// 64 bit
-// var FenrirFSPath = "......\\FenrirFS.exe"
-
-// 32 bit
-var FenrirFSPath = "......\\FenrirFS.exe"
+// FenrirFSパス
+var FenrirFSPath = "......";
+var FenrirFSPathExe = FenrirFSPath + "\\FenrirFS.exe";
+var FenrirFSPathSC = FenrirFSPath + "\\shortcut";
 
 // プロファイル名
 var ProfileName = "個人用";
 
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var wsh = new ActiveXObject("WScript.Shell");
+
+// 引数判定
 var args = WScript.Arguments;
 if ( args.length <= 0 ) {
   return;
 }
 
-var wsh = new ActiveXObject("WScript.Shell");
+// フォルダーショートカット用フォルダー作成
+if(!fso.FolderExists(FenrirFSPathSC)){
+  fso.CreateFolder(FenrirFSPathSC);
+}
+
+// ディレクトリorファイル判定
+var isDir = function(path){
+  if(fso.FolderExists(path)){
+    return true;
+  }else if(fso.FileExists(path)){
+    return false;
+  }else{
+    return false;
+  }
+}
+
 for(var i=0; i<args.length; i++){
-  var cmdline = '"' + FenrirFSPath + '" --profile "' + ProfileName + '" "' + args(i) + '"';
-  wsh.Run(cmdline);
+  if(isDir(args(i))){
+    var fileName = fso.GetFileName(args(i));
+    var scName = FenrirFSPathSC + "\\" + fileName + ".lnk";
+    var sc = wsh.CreateShortcut(scName);
+    sc.TargetPath = "file:/" + args(i);
+    sc.Save();
+    var cmdline = '"' + FenrirFSPathExe + '" --profile "' + ProfileName + '" "' + scName + '"';
+    wsh.Run(cmdline);
+  }else{
+    var cmdline = '"' + FenrirFSPathExe + '" --profile "' + ProfileName + '" --keep "' + args(i) + '"';
+    wsh.Run(cmdline);
+  }
 }
 
 })();
